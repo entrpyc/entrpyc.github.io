1efc151f9ddd094e15ee86a7cfbc02aed554fd57
asen <asen@grind.bg>
2020-10-09 11:59:22 +0300

    dir update

---
+++ demo-project/precache-manifest.588c5784239f29c1c469f782d7bc2426.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db0100c1267f1ebc2f92a9622cba5c94",
+    "url": "/demo-project/index.html"
+  },
+  {
+    "revision": "893785490ca5c4928574",
+    "url": "/demo-project/static/css/main.835e3a14.chunk.css"
+  },
+  {
+    "revision": "e2c0d435a06e62d88ecc",
+    "url": "/demo-project/static/js/2.d075c391.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/demo-project/static/js/2.d075c391.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "893785490ca5c4928574",
+    "url": "/demo-project/static/js/main.ca479a46.chunk.js"
+  },
+  {
+    "revision": "80b71a34b2942d974174",
+    "url": "/demo-project/static/js/runtime-main.bda79b85.js"
+  }
+]);
